6bd4d6b671beb8d3646ff4aba43bdbe2263a8907
yuvraj034c <yuvraj108c@gmail.com>
2019-02-18 17:06:46 +0400

    Added msg support for form

--- assets/js/course-handler.js
+++ assets/js/course-handler.js
@@ -11,8 +11,41 @@ function sendCourse(e) {
     var data = new FormData(courseForm);
 
     http.post("../includes/handlers/upload-course-handler.php", data)
-        .then(res => console.log(res))
-        .catch(err => console.log(err));
+        .then(res => {
+            console.log(res);
+
+            if (res === "Success") {
+                hideMsg("msgError");
+                showMsg("Course upload was successful", false);
+
+
+            } else {
+                let msg = "";
+                res.forEach(e => {
+                    msg += e + "</br>";
+                });
+                // Show Error msg
+                showMsg(msg, true);
+            }
+        })
+        .catch(err => showMsg(err, true));
 
     e.preventDefault();
+}
+
+function showMsg(msg, err) {
+    let box;
+    if (err) {
+        box = document.getElementById('msgError');
+    } else {
+        box = document.getElementById('msgSuccess');
+    }
+    box.classList.remove('hidden');
+    box.innerHTML = msg;
+}
+
+function hideMsg(id) {
+    let box = document.getElementById(id);
+    box.innerHTML = "";
+    box.classList.add("hidden");
 }
