89e440067fa7477f2465bbe593dd32240758a992
yuvraj034c <yuvraj108c@gmail.com>
2019-02-18 13:45:13 +0400

    Send post request to php file

---
+++ assets/js/course-handler.js
@@ -0,0 +1,15 @@
+// Fetch Library
+const http = new EasyHTTP();
+
+let courseForm = document.getElementById('courseForm');
+courseForm.addEventListener('submit', sendCourse);
+
+function sendCourse(e) {
+    var data = new FormData(courseForm);
+
+    http.post("../includes/handlers/upload-course-handler.php", data)
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+
+    e.preventDefault();
+}
