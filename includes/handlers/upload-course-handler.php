df4922a8843a0c31a970e16a3fbdb3f57c098aba
yuvraj034c <yuvraj108c@gmail.com>
2019-02-18 15:35:12 +0400

    upload handler working

--- includes/handlers/upload-course-handler.php
+++ includes/handlers/upload-course-handler.php
@@ -14,18 +14,17 @@ $benefit = strip_tags($_POST['benefit']);
 $requirements = strip_tags($_POST['requirements']);
 $description = strip_tags($_POST['description']);
 $target = strip_tags($_POST['target']);
+
 $videos = $_FILES['videos'];
 
-// $numOfVideos = count($videos['name']);
-
-// echo(count($videos["name"]));
+$numOfVideos = count($videos['name']);
 
 if ($course->validateAll($title,$videos) == true){
     // valid title & videos
 
-    if($course->insertDb($title,$category,$teaser,$benefit,$requirements,$description,$target) == true){
+    if($course->insertDb($title,$category,$teaser,$benefit,$requirements,$description,$target,$numOfVideos) == true){
         // insertion to db success
-        echo "success";
+        echo json_encode("success");
     }else{
         $errorArray = $course->getErrors();
         array_push($errorArray, "Error inserting data into database!");
