41374a5bf8baf163121920189c2b596389fb22f2
yuvraj034c <yuvraj108c@gmail.com>
2019-02-21 19:10:10 +0400

    Created navbar 2

---
+++ includes/components/navbar2.php
@@ -0,0 +1,29 @@
+
+<!-- Navbar -->
+<nav class="ui menu">
+    <div class="ui container">
+
+        <!-- Logo -->
+        <div class="logo">
+            <a href="http://localhost/E-Learning-2/pages/dashboard.php"><span id="subLogo">fre&nbsp;</span>E-Learning</a>
+        </div>
+
+        <!-- Search field -->
+        <form class="ui search icon input" id="searchForm" >
+            <input class="prompt" type="text" placeholder="Search posts by sector" id="searchField">
+            <i class="search icon" id="searchIcon"></i>
+            <div class="results"></div>
+        </form>
+            
+        <!-- User Space -->
+        <div class="ui pointing dropdown" id="userDrop" tabindex="0">
+                <span class="text" id="user"><?php echo $_SESSION['userLoggedIn']; ?></span>
+                <i class="dropdown icon"></i>
+                <div class="menu" tabindex="-1">
+                    <a class="item" href="http://localhost/E-Learning-2/index.php">Log out</a>
+                    <a class="item" href="#">Edit Profile</a>
+                </div>
+        </div>
+    </div>
+</nav>
+
