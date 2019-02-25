c65a3820f66acbf3a627b215b0a434fd76df1ee4
yuvraj034c <yuvraj108c@gmail.com>
2019-02-25 19:59:12 +0400

    Create class for messages

---
+++ includes/classes/Messages.php
@@ -0,0 +1,22 @@
+<?php
+class Messages{
+	public static function setMsg($text, $type){
+		if($type == 'error'){
+			$_SESSION['errorMsg'] = $text;
+		} else {
+			$_SESSION['successMsg'] = $text;
+		}
+	}
+
+	public static function display(){
+		if(isset($_SESSION['errorMsg'])){
+			echo '<div class="ui red message">'.$_SESSION['errorMsg'].'</div>';
+			unset($_SESSION['errorMsg']);
+		}
+
+		if(isset($_SESSION['successMsg'])){
+			echo '<div class="ui blue message">'.$_SESSION['successMsg'].'</div>';
+			unset($_SESSION['successMsg']);
+		}
+	}
+}
