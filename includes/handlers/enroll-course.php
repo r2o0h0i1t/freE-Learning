43e4911d50a4f58e9f3f7259c901fc38bfe9eb6e
yuvraj034c <yuvraj108c@gmail.com>
2019-02-26 15:35:02 +0400

    Created validation for user logged in

--- includes/handlers/enroll-course.php
+++ includes/handlers/enroll-course.php
@@ -2,16 +2,24 @@
 
 include('../config.php');
 
-$userId = $_SESSION['userLoggedInId'];
-$courseId = $_POST['enrollBtn'];
+if(isset($_SESSION['userLoggedInName'])){
 
-$date = date("Y/m/d");
-
-$query = "INSERT INTO enrolled VALUES ('$userId','$courseId','$date')";
-
-$result = mysqli_query($con,$query);
-if($result){
-    header("Location: ../../dashboard.php");
+    $username = $_SESSION['userLoggedInName'];
+    $result = mysqli_query($con, "SELECT * FROM users WHERE username = '$username'");
+    $userId = mysqli_fetch_assoc($result)['id'];
+    
+    $courseId = $_POST['enrollBtn'];
+    
+    $date = date("Y/m/d");
+    
+    $query = "INSERT INTO enrolled VALUES ('$userId','$courseId','$date')";
+    
+    $result = mysqli_query($con,$query);
+    if($result){
+        header("Location: ../../dashboard.php");
+    }else{
+        echo "Error inserting to database!";
+    }
 }else{
-    echo "Error inserting to database!";
+    header("Location: ../../login.php");
 }
