d1c1822c3966055cae348f311dc402d768f6cea1
yuvraj034c <yuvraj108c@gmail.com>
2019-02-26 13:59:15 +0400

    Created register-handler

---
+++ includes/handlers/register-handler.php
@@ -0,0 +1,77 @@
+<?php
+
+include("includes/config.php");
+include("includes/classes/Messages.php");
+include("includes/classes/Account.php");
+include("includes/classes/Constants.php");
+
+if(isset($_POST['registerBtn'])){
+
+if($_POST['g-recaptcha-response'] == ""){
+    Messages::setMsg("Please Check captcha box","error");
+}else{
+
+    $account = new Account($con);
+
+    // Remove html tags
+    $fname = strip_tags($_POST['fname']);
+    $lname = strip_tags($_POST['lname']);
+    $username = strip_tags($_POST['username']);
+    $email = strip_tags($_POST['email']);
+    $pwd = strip_tags($_POST['pwd']);
+    $pwd2 = strip_tags($_POST['pwd2']);
+    $img = $_FILES['img'];
+
+
+    // Recaptcha response
+    $responseKey = $_POST['g-recaptcha-response'];
+
+    if($account->validateAll($fname,$lname,$username,$email,$pwd,$pwd2,$img) == true){ 
+        // Input values are valid
+        
+        if($account->validateCaptcha($responseKey) == true){ 
+            // Captcha response is valid
+
+            $file_extension = strtolower(pathinfo($img["name"], PATHINFO_EXTENSION));
+            $path = "assets//images//profilePictures//" .$username.'.' .$file_extension;
+
+            $imgMoved = move_uploaded_file($img['tmp_name'], $path);
+
+            if($imgMoved){
+                if($account->register($fname,$lname,$username,$email,$pwd) == true){ 
+
+                    $_SESSION['userLoggedInName'] = $username;
+
+                    $result = mysqli_query($con, "SELECT * FROM users WHERE username = '$username'");
+                    $arr = mysqli_fetch_assoc($result);
+                    $_SESSION['userLoggedInId'] = $arr['id'];
+                    
+                    // Go to dashboard
+                    header("Location: dashboard.php");
+
+                    
+                }else{
+                    $errorArray = $account->getErrors();
+                    array_push($errorArray, "Error inserting data into database!");
+                    Messages::setMsg($errorArray[0],"error");
+                }
+            }else{
+                $errorArray = $account->getErrors();
+                array_push($errorArray, "Failed to move uploaded image!");
+                Messages::setMsg($errorArray[0],"error");
+            }
+        }else{
+            $errorArray = $account->getErrors();
+            array_push($errorArray, "Recaptcha verification failed!");
+            Messages::setMsg($errorArray[0],"error");
+        }
+    }else{
+        // Output all errors
+        $msg = '';
+        foreach ($account->getErrors() as $error) {
+            $msg= $msg . $error."</br>";
+        }
+        Messages::setMsg($msg,"error");
+    }
+}
+}
