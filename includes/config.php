c5a6ae1b15240ada7e8b81267e6aee1b5c780ff7
Unknown <yuvraj108c@gmail.com>
2019-02-28 20:39:36 +0400

    Added comments

--- includes/config.php
+++ includes/config.php
@@ -2,13 +2,17 @@
 	ob_start();
 	session_start();
 
+	// Set timezone
 	$timezone = date_default_timezone_set("Indian/Mauritius");
 
+	// db connection
 	$con = mysqli_connect("localhost", "root", "", "e-learning");
 
+	// Check if db connection success
 	if(mysqli_connect_errno()) {
-		echo "Failed to connect: " . mysqli_connect_errno();
+		die("Failed to connect: " . mysqli_connect_errno());
 	}
 
+	// Constant for root url
 	define("ROOT_URL", "http://localhost/freE-Learning/");
 ?>
