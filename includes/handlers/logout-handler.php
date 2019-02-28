6a766c7e7c08c7c6297e74df08e2eb4b1d9a346e
Unknown <yuvraj108c@gmail.com>
2019-02-28 20:29:47 +0400

    Added comments

--- includes/handlers/logout-handler.php
+++ includes/handlers/logout-handler.php
@@ -1,6 +1,9 @@
 <?php
 include("../config.php");
+
+// Clear all sessions variables
 unset($_SESSION['userLoggedInName']);
 unset($_SESSION['userLoggedInId']);
+
+// Go to homepage
 header("Location: ". ROOT_URL . "homepage.php");
-?>
