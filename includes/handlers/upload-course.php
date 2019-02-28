f0709f75b76064b97ae170e91328c45deeb7d23e
Unknown <yuvraj108c@gmail.com>
2019-02-28 20:15:26 +0400

    Added more comments

--- includes/handlers/upload-course.php
+++ includes/handlers/upload-course.php
@@ -5,11 +5,12 @@ include("includes/classes/Course.php");
 include("includes/classes/Constants.php");
 include("includes/classes/Messages.php");
 
+// Check if form is submitted
 if(isset($_POST['submit'])){
     
     $course = new Course($con);
     
-        // Remove html tags
+    // Remove html tags from inputs
     $title = strip_tags($_POST['title']);
     $category = strip_tags($_POST['category']);
     $teaser = strip_tags($_POST['teaser']);
@@ -18,17 +19,20 @@ if(isset($_POST['submit'])){
     $description = strip_tags($_POST['description']);
     $target = strip_tags($_POST['target']);
 
+    // Uploaded files
     $videos = $_FILES['videos'];
     $image = $_FILES['image'];
 
+    // Get num of videos uploaded
     $numOfVideos = count($videos['name']);
 
+    // Validate title, videos, image
     if ($course->validateAll($title,$videos,$image) == true){
-        // valid title & videos
         
+        // Check if insertion to db success
         if($course->insertDb($title,$category,$teaser,$benefit,$requirements,$description,$target,$numOfVideos) == true){
-            // insertion to db success
 
+            // Check if video titles inserted into videos table
             if($course->insertVideos($title,$videos) == true){
                 
                 // move videos to server
@@ -37,8 +41,11 @@ if(isset($_POST['submit'])){
                 // move img to server
                 $course->moveImage($image,$title);
                 
+                // Success msg
                 Messages::setMsg("Course uploaded Successfully","success");
+                
             }else{
+
                 Messages::setMsg("Error inserting videos data into database!","error");
             }
             
@@ -46,8 +53,10 @@ if(isset($_POST['submit'])){
             Messages::setMsg("Error inserting data into database!","error");
         }
     }else{
-        // Output all errors
+        // tmp variable: store all errors
         $msg = '';
+
+        // Get all messages from error array
         foreach ($course->getErrors() as $error) {
             $msg= $msg . $error."</br>";
         }
