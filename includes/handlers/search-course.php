0827e86507272727b91ef2df25a491ca1f53a518
yuvraj034c <yuvraj108c@gmail.com>
2019-03-14 22:19:35 +0400

    created search result handler

---
+++ includes/handlers/search-course.php
@@ -0,0 +1,17 @@
+<?php
+include('../config.php');
+
+if(isset($_POST["search-value"])){
+    $search = $_POST["search-value"];
+
+    $query = "SELECT * FROM course WHERE title LIKE '%$search%'";
+    $result = mysqli_query($con,$query);
+
+    $courses = array();
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        array_push($courses,$row['title']);
+    }
+
+    echo json_encode($courses);
+}
