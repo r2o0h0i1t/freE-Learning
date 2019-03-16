f16ee68e31b567639361a3cc26ec8848b92a8874
yuvraj034c <yuvraj108c@gmail.com>
2019-03-16 19:56:43 +0400

    Checked username using ajax

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,4 +1,5 @@
 const usernameInput = document.getElementById('username');
+const messageBox = document.getElementById('messages');
 
 usernameInput.addEventListener("keyup", checkUsername);
 
@@ -6,26 +7,56 @@ function checkUsername(e) {
     const field = usernameInput.parentElement;
 
     const username = e.target.value;
+    messageBox.innerHTML = "";
+
     if (username.length > 0) {
-        showLoadingIcon(field)
+        // Errors in username
+        showLoadingIcon(field);
+
+        setTimeout(() => {
+            let http = new XMLHttpRequest();
+            http.open("POST", "includes/handlers/ajax/check-username.php", true);
+            http.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+            http.onload = () => {
+                let errors = JSON.parse(http.responseText);
+                if (errors.length > 0) {
+                    let output = ""
+                    errors.forEach(error => {
+                        output += '<div class="ui red message">' + error + '</div>';
+                    });
+                    field.parentElement.classList.add("error");
+                    messageBox.innerHTML = "";
+                    messageBox.innerHTML = output;
+                    hideLoadingIcon(field);
+                    hideCheckIcon(field);
+                } else {
+                    field.parentElement.classList.remove("error");
+                    messageBox.innerHTML = "";
+                    hideLoadingIcon(field);
+                    showCheckIcon(field);
+                }
+            }
+            let data = "username=" + username;
+            http.send(data);
+        }, 1500);
     } else {
+        field.parentElement.classList.remove("error");
         hideLoadingIcon(field);
+        hideCheckIcon(field);
     }
 
 }
 
 function showLoadingIcon(field) {
-    // Load loading icon
     field.querySelector("i.check.icon").style.display = "none";
     field.querySelector("i.notched.circle.loading.icon").style.display = "block";
 }
-
 function hideLoadingIcon(field) {
     field.querySelector("i.notched.circle.loading.icon").style.display = "none";
 }
 function showCheckIcon(field) {
-    // Load success icon
-    field.querySelector("i.notched.circle.loading.icon").style.display = "none";
     field.querySelector("i.check.icon").style.display = "block";
 }
-
+function hideCheckIcon(field) {
+    field.querySelector("i.check.icon").style.display = "none";
+}
