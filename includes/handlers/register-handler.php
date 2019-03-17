3befff1d28ef429192e0efe89d78543ed08b41e6
yuvraj034c <yuvraj108c@gmail.com>
2019-03-17 09:06:39 +0400

    Modified existing handler

--- includes/handlers/register-handler.php
+++ includes/handlers/register-handler.php
@@ -1,83 +1,60 @@
 <?php
 
-include("includes/config.php");
-include("includes/classes/Messages.php");
-include("includes/classes/Account.php");
-include("includes/classes/Constants.php");
+include("../config.php");
+include("../classes/Account.php");
+include("../classes/Constants.php");
 
-// Check if register form is submitted
-if(isset($_POST['registerBtn'])){
 
-    // Check if user checked recaptcha box
-    if($_POST['g-recaptcha-response'] == ""){
-        // Set error msg
-        Messages::setMsg("Please Check captcha box","error");
-    }else{
-        // New account object
-        $account = new Account($con);
+$fname = strip_tags($_POST['fname']);
+$lname = strip_tags($_POST['lname']);
+$username = strip_tags($_POST['uname']);
+$email = strip_tags($_POST['email']);
+$pwd1 = strip_tags($_POST['pwd1']);
+$pwd2 = strip_tags($_POST['pwd2']);
+$recaptchaResponse = $_POST['g-recaptcha-response'];
+$img = $_FILES['img'];
 
-        // Remove html tags
-        $fname = strip_tags($_POST['fname']);
-        $lname = strip_tags($_POST['lname']);
-        $username = strip_tags($_POST['username']);
-        $email = strip_tags($_POST['email']);
-        $pwd = strip_tags($_POST['pwd']);
-        $pwd2 = strip_tags($_POST['pwd2']);
-        
-        // Profile picture
-        $img = $_FILES['img'];
 
-        // Recaptcha response
-        $responseKey = $_POST['g-recaptcha-response'];
-
-        // Validate all inputs
-        if($account->validateAll($fname,$lname,$username,$email,$pwd,$pwd2,$img) == true){ 
-            
-            // Validate captcha response
-            if($account->validateCaptcha($responseKey) == true){ 
-
-                // Get image extension: convert to lcase first
-                $file_extension = strtolower(pathinfo($img["name"], PATHINFO_EXTENSION));
-
-                // Set destination path for image
-                $path = "assets//images//profilePictures//" .$username.'.' .$file_extension;
-
-                // Move img to path
-                $imgMoved = move_uploaded_file($img['tmp_name'], $path);
-
-                // Check if img moved correctly
-                if($imgMoved){
-
-                    // Check if data was inserted to db correctly
-                    if($account->register($fname,$lname,$username,$email,$pwd) == true){ 
-
-                        // Create session for user
-                        $_SESSION['userLoggedInName'] = $username;
-                    
-                        // Go to dashboard
-                        header("Location: dashboard.php");
-                        
-                    }else{
-
-                        Messages::setMsg("Error inserting data into database!","error");
-                    }
-                }else{
-
-                    Messages::setMsg("Failed to move uploaded image!","error");
-                }
-            }else{
-
-                Messages::setMsg("Recaptcha verification failed!","error");
-            }
-        }else{
-            $msg = '';
-            
-            // Output all errors
-            foreach ($account->getErrors() as $error) {
-                $msg= $msg . $error."</br>";
-            }
-            
-            Messages::setMsg($msg,"error");
-        }
+    if($recaptchaResponse == ""){
+        echo json_encode("Please check recaptcha box");
+        return;
     }
-}
+    
+    $account = new Account($con);
+
+    // Recaptcha verification
+    if($account->validateCaptcha($recaptchaResponse) == false){
+        echo json_encode("Recaptcha verification failed");
+        return;
+    }
+
+    if($account->validateImage($img) == false){
+        echo json_encode($account->getErrors());
+        return;
+    }
+
+    // Get image extension: convert to lcase first
+    $file_extension = strtolower(pathinfo($img["name"], PATHINFO_EXTENSION));
+
+    // Set destination path for image
+    $path = "..//..//assets//images//profilePictures//" .$username.'.' .$file_extension;
+
+    // Move img to path
+    $imgMoved = move_uploaded_file($img['tmp_name'], $path);
+
+    // Check if img moved correctly
+    if(!$imgMoved){
+        echo json_encode("Image not saved to server");
+        return;
+    }
+
+    // Check if data was inserted to db correctly
+    if($account->register($fname,$lname,$username,$email,$pwd1) == false){ 
+        echo json_encode("Error inserting data to database");
+        return;
+    }
+
+    // Create session for user
+    $_SESSION['userLoggedInName'] = $username;
+
+    echo json_encode("success");
