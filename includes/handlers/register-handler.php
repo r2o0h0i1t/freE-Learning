c4637e64e3d764092fd9bd8ec44f5af92b3ac621
yuvraj034c <yuvraj108c@gmail.com>
2019-03-18 19:22:14 +0400

    Closed db connection

--- includes/handlers/register-handler.php
+++ includes/handlers/register-handler.php
@@ -1,60 +1,65 @@
 <?php
 
-include("../config.php");
-include("../classes/Account.php");
-include("../classes/Constants.php");
+require("../config.php");
+require("../classes/Account.php");
+require("../classes/Constants.php");
+
+// Remove tags
+$fname = strip_tags($_POST["fname"]);
+$lname = strip_tags($_POST["lname"]);
+$username = strip_tags($_POST["uname"]);
+$email = strip_tags($_POST["email"]);
+$pwd1 = strip_tags($_POST["pwd1"]);
+$pwd2 = strip_tags($_POST["pwd2"]);
+
+$recaptchaResponse = $_POST["g-recaptcha-response"];
+
+// Profile picture
+$img = $_FILES["img"];
 
 
-$fname = strip_tags($_POST['fname']);
-$lname = strip_tags($_POST['lname']);
-$username = strip_tags($_POST['uname']);
-$email = strip_tags($_POST['email']);
-$pwd1 = strip_tags($_POST['pwd1']);
-$pwd2 = strip_tags($_POST['pwd2']);
-$recaptchaResponse = $_POST['g-recaptcha-response'];
-$img = $_FILES['img'];
+if($recaptchaResponse == ""){
+    echo json_encode("Please check recaptcha box");
+    return;
+}
 
+$account = new Account($con);
 
-    if($recaptchaResponse == ""){
-        echo json_encode("Please check recaptcha box");
-        return;
-    }
-    
-    $account = new Account($con);
+// Recaptcha verification
+if($account->validateCaptcha($recaptchaResponse) == false){
+    echo json_encode("Recaptcha verification failed");
+    return;
+}
 
-    // Recaptcha verification
-    if($account->validateCaptcha($recaptchaResponse) == false){
-        echo json_encode("Recaptcha verification failed");
-        return;
-    }
+if($account->validateImage($img) == false){
+    echo json_encode($account->getErrors());
+    return;
+}
 
-    if($account->validateImage($img) == false){
-        echo json_encode($account->getErrors());
-        return;
-    }
+// Get image extension
+$file_extension = strtolower(pathinfo($img["name"], PATHINFO_EXTENSION));
 
-    // Get image extension: convert to lcase first
-    $file_extension = strtolower(pathinfo($img["name"], PATHINFO_EXTENSION));
+// Set destination path
+$path = "..//..//assets//images//profilePictures//" .$username.'.' .$file_extension;
 
-    // Set destination path for image
-    $path = "..//..//assets//images//profilePictures//" .$username.'.' .$file_extension;
+// Move img to path
+$imageWasMoved = move_uploaded_file($img['tmp_name'], $path);
 
-    // Move img to path
-    $imgMoved = move_uploaded_file($img['tmp_name'], $path);
+if(!$imageWasMoved){
+    echo json_encode("Image not saved to server");
+    return;
+}
 
-    // Check if img moved correctly
-    if(!$imgMoved){
-        echo json_encode("Image not saved to server");
-        return;
-    }
+// Check if data was inserted to db correctly
+if($account->register($fname,$lname,$username,$email,$pwd1) == false){ 
+    echo json_encode("Error inserting data to database");
+    return;
+}
 
-    // Check if data was inserted to db correctly
-    if($account->register($fname,$lname,$username,$email,$pwd1) == false){ 
-        echo json_encode("Error inserting data to database");
-        return;
-    }
+// Create session
+$_SESSION['userLoggedInName'] = $username;
 
-    // Create session for user
-    $_SESSION['userLoggedInName'] = $username;
+// Close db connection
+mysqli_close($con);
 
-    echo json_encode("success");
+echo json_encode("success");
