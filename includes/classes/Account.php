9ea0b48bc091b1e416216c8419ff675c2ae9a1c8
yuvraj034c <yuvraj108c@gmail.com>
2019-03-18 19:40:33 +0400

    Refactored code

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -2,19 +2,19 @@
 
 class Account{
     private $con; // db connection
-    private $errorArray; // Array to hold all errors
+    private $errorArray;
 
     public function __construct($con) {
         $this->con = $con;
         $this->errorArray = array();
     }
 
-    public function validateUserLogin($un,$pw){
-        // Md5 encryption of password
-        $pw = md5($pw);
+    public function validateUserLogin($username,$password){
+        // Md5 encryption
+        $password = md5($password);
 
         // Check if username & password exists
-        $query = mysqli_query($this->con, "SELECT * FROM users WHERE username='$un' AND pwd='$pw'");
+        $query = mysqli_query($this->con, "SELECT * FROM users WHERE username='$username' AND pwd='$password'");
         
         if(mysqli_num_rows($query) == 1) {
             return true;
@@ -25,14 +25,14 @@ class Account{
         }
     }
 
-    public function validateUsername($un) {
+    public function validateUsername($username) {
         // Check length
-        if(strlen($un) > 25 || strlen($un) < 5) {
+        if(strlen($username) > 25 || strlen($username) < 5) {
             array_push($this->errorArray, Constants::$usernameCharacters);
             return;
         }
         // Check if username exists
-        $checkUsernameQuery = mysqli_query($this->con, "SELECT username FROM users WHERE username='$un'");
+        $checkUsernameQuery = mysqli_query($this->con, "SELECT username FROM users WHERE username='$username'");
 
         if(mysqli_num_rows($checkUsernameQuery) != 0) {
             array_push($this->errorArray, Constants::$usernameTaken);
@@ -41,31 +41,31 @@ class Account{
     
     }
 
-    public function validateFirstName($fn) {
+    public function validateFirstName($firstName) {
         // Check length
-        if(strlen($fn) > 25 || strlen($fn) < 2) {
+        if(strlen($firstName) > 25 || strlen($firstName) < 2) {
             array_push($this->errorArray, Constants::$firstNameCharacters);
             return;
         }
     }
     
-    public function validateLastName($ln) {
+    public function validateLastName($lastName) {
         // Check length
-        if(strlen($ln) > 25 || strlen($ln) < 2) {
+        if(strlen($lastName) > 25 || strlen($lastName) < 2) {
             array_push($this->errorArray, Constants::$lastNameCharacters);
             return;
         }
     }
 
-    public function validateEmail($em) {
+    public function validateEmail($email) {
         // Validate email
-        if(!filter_var($em, FILTER_VALIDATE_EMAIL)) {
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             array_push($this->errorArray, Constants::$emailInvalid);
             return;
         }
 
-        // Check if email exists
-        $checkEmailQuery = mysqli_query($this->con, "SELECT email FROM users WHERE email='$em'");
+        // Check if email already exists
+        $checkEmailQuery = mysqli_query($this->con, "SELECT email FROM users WHERE email='$email'");
 
         if(mysqli_num_rows($checkEmailQuery) != 0) {
             array_push($this->errorArray, Constants::$emailTaken);
@@ -74,28 +74,22 @@ class Account{
     
     }
  
-    public function validatePasswords($pw) {
+    public function validatePassword($password) {
         // Check if password is alphanumeric
-        if(preg_match('/[^A-Za-z0-9]/', $pw)) {
+        if(preg_match('/[^A-Za-z0-9]/', $password)) {
             array_push($this->errorArray, Constants::$passwordNotAlphanumeric);
             return;
         }
         // Check length
-        if(strlen($pw) > 30 || strlen($pw) < 5) {
+        if(strlen($password) > 30 || strlen($password) < 5) {
             array_push($this->errorArray, Constants::$passwordCharacters);
             return;
         }
-        // Check if same passwords
-        // if($pw != $pw2) {
-        //     array_push($this->errorArray, Constants::$passwordsDoNoMatch);
-        //     return;
-        // }
-    
     }
 
-    public function validateImage($img){
+    public function validateImage($image){
         // Select file type
-        $file_extension = strtolower(pathinfo($img["name"], PATHINFO_EXTENSION));
+        $file_extension = strtolower(pathinfo($image["name"], PATHINFO_EXTENSION));
     
         // Valid file extensions
         $imageFileType = array("jpg");
@@ -106,13 +100,11 @@ class Account{
             return;
         }
         // Check size of img
-        if (($img["size"] > 2000000)) {
+        if (($image["size"] > 2000000)) {
             array_push($this->errorArray, Constants::$imageTooLarge);
             return;
         }
-
         return true;
-    
     }
 
     public function validateCaptcha($responseKey){
@@ -125,33 +117,20 @@ class Account{
         $response = file_get_contents($url);
         $response = json_decode($response);
 
-        // If success
+        // Check response
         return $response->success;
     }
 
-    public function register($fn,$ln,$un,$em,$pw){
+    public function register($firstName,$lastName,$username,$email,$password){
         // Encrypt password
-        $encryptedPw = md5($pw);
+        $encryptedPwd = md5($password);
         $date = date("Y/m/d");
     
         // Insert values to db
-        $result = mysqli_query($this->con, "INSERT INTO users VALUES ('', '$fn', '$ln', '$un','$em', '$encryptedPw','$date')");
+        $result = mysqli_query($this->con, "INSERT INTO users VALUES ('', '$firstName', '$lastName', '$username','$email', '$encryptedPwd','$date')");
         return $result;
     }
     
-    // public function validateAll($fn,$ln,$un,$em,$p1,$p2,$img){
-    //     // Validate all inputs
-
-    //     $this->validateFirstName($fn);
-    //     $this->validateLastName($ln);
-    //     $this->validateUsername($un);
-    //     $this->validateEmail($em);
-    //     $this->validatePasswords($p1,$p2);
-    //     $this->validateImage($img);
-
-    //     return empty($this->errorArray);
-    // }
-
     public function getErrors(){
         return $this->errorArray;
     }
