7a539c9c345d0d72143a382d25a0305c568595ce
yuvraj034c <yuvraj108c@gmail.com>
2019-03-18 18:53:23 +0400

    Added comments

--- includes/handlers/ajax/register-form-checks.php
+++ includes/handlers/ajax/register-form-checks.php
@@ -6,6 +6,7 @@ require("../../classes/Account.php");
 
 $account = new Account($con);
 
+// Username
 if(isset($_POST["uname"])){
     $uname = $_POST["uname"];
     $account->validateUsername($uname);
@@ -13,6 +14,8 @@ if(isset($_POST["uname"])){
     $errors = $account->getErrors();
     echo json_encode($errors);
 }
+
+// First name
 if(isset($_POST["fname"])){
     $fname = $_POST["fname"];
     $account->validateFirstName($fname);
@@ -20,6 +23,8 @@ if(isset($_POST["fname"])){
     $errors = $account->getErrors();
     echo json_encode($errors);
 }
+
+// Last name
 if(isset($_POST["lname"])){
     $lname = $_POST["lname"];
     $account->validateLastName($lname);
@@ -27,6 +32,8 @@ if(isset($_POST["lname"])){
     $errors = $account->getErrors();
     echo json_encode($errors);
 }
+
+// Email
 if(isset($_POST["email"])){
     $email = $_POST["email"];
     $account->validateEmail($email);
@@ -34,6 +41,8 @@ if(isset($_POST["email"])){
     $errors = $account->getErrors();
     echo json_encode($errors);
 }
+
+// Password 1
 if(isset($_POST["pwd1"])){
     $pwd1 = $_POST["pwd1"];
     $account->validatePasswords($pwd1);
@@ -41,6 +50,8 @@ if(isset($_POST["pwd1"])){
     $errors = $account->getErrors();
     echo json_encode($errors);
 }
+
+// Password 2
 if(isset($_POST["pwd2"])){
     $pwd2 = $_POST["pwd2"];
     $account->validatePasswords($pwd2);
