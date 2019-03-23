5235c9e7b99515efd1f625aa5cc4e27b0130492f
yuvraj034c <yuvraj108c@gmail.com>
2019-03-23 11:56:23 +0400

    Display upload btn if admin logged in

--- includes/components/navbar.php
+++ includes/components/navbar.php
@@ -41,29 +41,48 @@
         <?php
             // Check if user is logged in
             if (!isset($_SESSION['userLoggedInName'])) {
+                
+                // Check if admin ?
+                if (!isset($_SESSION['adminLoggedInName'])){
 
-                // Display login button
-                echo "<a class='ui basic grey button' id='loginBtn' href='login.php'>Log In</a>";
-                echo "<a class='ui primary button' id='signUpBtn' href='register.php'>Sign up</a>";
+                    // Display login button
+                    echo "<a class='ui basic grey button' id='loginBtn' href='login.php'>Log In</a>";
+                    echo "<a class='ui primary button' id='signUpBtn' href='register.php'>Sign up</a>";
 
-            } else {
+                }else{
+                    // Admin logged in
+                    echo "<a class='ui basic button' href='upload.php' id='uploadBtn'>Upload</a>";
 
-                echo "<a class='ui basic button' href='upload.php' id='uploadBtn'>Upload</a>";
-                //Display dropdown with options..
-                echo
+                    echo
                     "<div class='ui pointing dropdown' id='userDrop' tabindex='0'>
-                                <div class='text'>
-                                    <img class='ui avatar image' src='assets/images/profilePictures/" . $_SESSION['userLoggedInName'] . ".jpg'>" .
-                    $_SESSION['userLoggedInName'] .
+                                <div class='text'>". $_SESSION['adminLoggedInName'] .
                     "</div>
                                 <i class='dropdown icon'></i>
                                 <div class='menu' tabindex='-1'>
                                     <span class='text' id='user'></span>
-                                    <a class='item' href='dashboard.php'>My Courses</a>
-                                    <a class='item' href='#'>Edit Profile</a>
+                                    <a class='item' href='adminpanel.php'>View all Courses</a>
                                     <a class='item' href='includes/handlers/logout-handler.php'>Log out</a>
                                 </div>
                             </div>";
+                }
+                    
+            } else  {
+
+            //Display dropdown with options..
+            echo
+                "<div class='ui pointing dropdown' id='userDrop' tabindex='0'>
+                            <div class='text'>
+                                <img class='ui avatar image' src='assets/images/profilePictures/" . $_SESSION['userLoggedInName'] . ".jpg'>" .
+                $_SESSION['userLoggedInName'] .
+                "</div>
+                            <i class='dropdown icon'></i>
+                            <div class='menu' tabindex='-1'>
+                                <span class='text' id='user'></span>
+                                <a class='item' href='dashboard.php'>My Courses</a>
+                                <a class='item' href='#'>Edit Profile</a>
+                                <a class='item' href='includes/handlers/logout-handler.php'>Log out</a>
+                            </div>
+                        </div>";
             }
             ?>
     </div>
