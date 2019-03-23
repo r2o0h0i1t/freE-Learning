1986fd40da93a76bacba21e6fb3a829f49cf62a5
yuvraj034c <yuvraj108c@gmail.com>
2019-03-23 11:56:08 +0400

    Unset admin session

--- includes/handlers/logout-handler.php
+++ includes/handlers/logout-handler.php
@@ -4,6 +4,7 @@ include "../config.php";
 // Clear all sessions variables
 unset($_SESSION['userLoggedInName']);
 unset($_SESSION['userLoggedInId']);
+unset($_SESSION['adminLoggedInName']);
 
 // Go to homepage
 header("Location: " . ROOT_URL . "homepage.php");
