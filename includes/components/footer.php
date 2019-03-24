6580b0298271734f7f89fcb558e25b4be36c4d23
yuvraj034c <yuvraj108c@gmail.com>
2019-03-24 20:03:27 +0400

    Added media queries

--- includes/components/footer.php
+++ includes/components/footer.php
@@ -3,13 +3,13 @@
         <div class="ui grid">
 
             <!-- logo -->
-            <div class="six wide column">
+            <div class="eight wide column">
                 <a href="#"><span id="subLogo">fre&nbsp;</span>E-Learning</a>
                 <p id="copyright"> Copyright © 2018 fre E-Learning, Inc</p>
             </div>
 
             <!-- conditions -->
-            <div class="six wide column" id="conditions">
+            <div class="eight wide column" id="conditions">
                 <ul>
                     <li><a href="#">About</a></li>
                     <li><a href="#">Terms of use</a></li>
